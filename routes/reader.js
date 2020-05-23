0d11635b592b9876e16a556cb917a8ef759a19ba
Favour Ori <orifavour2000@gmail.com>
2020-05-23 09:59:56 +0200

    getting all readers

--- routes/reader.js
+++ routes/reader.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getAllReaders, createReader } = require("../controller/reader");
 
-router.get("/allReader", getAllReaders);
+router.get("/allReaders", getAllReaders);
 router.post("/", createReader);
 
 module.exports = router;
