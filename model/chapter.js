a62e040e15471025f5b93cecedbfb590a58a7833
Favour Ori <orifavour2000@gmail.com>
2020-06-14 03:09:10 +0200

    create chapter

---
+++ model/chapter.js
@@ -0,0 +1,13 @@
+let mongoose = require("mongoose");
+
+let chapterSchema = mongoose.Schema({
+  title: { type: String, required: [true, "course episode title is required"] },
+  story: { type: mongoose.Schema.Types.ObjectId, ref: "story" },
+  audioLink: String,
+  hasSeen: { type: Boolean, default: false },
+  views: { type: Number },
+});
+
+let Chapter = mongoose.model("chapter", chapterSchema);
+
+module.exports = Chapter;
